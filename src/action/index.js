a960eb2c0364a270346d68e8a54317f0194388a2
untari <sastraembun0306@gmail.com>
2021-07-02 23:02:05 +0700

    fetch json data from the local folder

--- src/action/index.js
+++ src/action/index.js
@@ -2,7 +2,7 @@ import {  FETCH_QUOTES, NEW_QUOTE } from '../Redux';
 
 // FETCH_QUOTES
 export const fetchQuotes = () => dispatch => {
-    fetch('https://gist.githubusercontent.com/irkreja/5f35dc197c6be4ddc32a45acdd23fcd7/raw/0d9b85d4a9d9da9dc67fa0257df66ed80702ff3a/quotes.json')
+    fetch('data/quotes.json')
     
     .then(response => {
       if (!response.ok) {
