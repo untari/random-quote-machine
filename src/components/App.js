9ff8b6fd1ec89137ca4afba8d21b224fcd42ec1a
untari <sastraembun0306@gmail.com>
2021-07-02 15:54:30 +0700

    wrap component with provider from react-redux

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 import RandomQuote from './randomQuote';
+import { Provider } from 'react-redux';
 import './App.css';
 
 class App extends Component{
   render(){
     return (
-        <div className="App">
+        <Provider>
             <RandomQuote />
-        </div>
+        </Provider>
     );
   }
 }
