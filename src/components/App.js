d7595e72f0f5a0536d6c359ce36419e7b0ab4de3
untari <sastraembun0306@gmail.com>
2021-07-02 16:01:10 +0700

    create store

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import RandomQuote from './randomQuote';
 import { Provider } from 'react-redux';
+import { store } from '../store';
 import './App.css';
 
 class App extends Component{
   render(){
     return (
-        <Provider>
+        <Provider store={store}>
             <RandomQuote />
         </Provider>
     );
